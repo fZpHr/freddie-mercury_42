d4e17dc169572b9511e1d823ddb323d9144ec4cf
fZpHr <afterrush@live.fr>
2024-12-18 20:50:59 +0100

    End mandatory part, and refactor structure + clean

--- models/bin/scene.gd
+++
@@ -1,6 +0,0 @@
-extends Node3D
-
-func _ready():
-	var anim_player = $AnimationPlayer  # Ajuste le chemin selon ta structure
-	anim_player.play("Count")
-	anim_player.get_animation("Count").loop_mode = Animation.LOOP_LINEAR
--- scenes/world/World.gd
+++
@@ -1,7 +0,0 @@
-extends Spatial
-
-onready var door = $Door
-
-func _on_Keypad_on_correct_password(password):
-	print("correct password")
-	door.open()
